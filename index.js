9402f67f22d134675015149f0f052494c68c6514
Zeth <zethsatentes@yahoo.com>
2022-04-21 16:17:47 -0700

    Added const variables with needed packages

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+
+const generatorMarkdown = require("./utils/generateMarkdown");
+
+//Questions for user 
